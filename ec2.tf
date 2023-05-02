93bd06da2ebf75e6f784c2a0150f3e37a12f9f9e
CodingManoj <mvp2612@hotmail.com>
2023-05-01 23:18:46 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,26 @@
 # Creates a SPOT Reqeuest : Creatin a SPOT Reqeust means, it's going to place a request and based on the availability AWS will grant you an EC2 Server of type SPOT.
 resource "aws_spot_instance_request" "spot" {
-  count                 = var.SPOT_INSTANCE_COUNT  
-  ami                   = data.aws_ami.ami.id
-  instance_type         = var.INSTANCE_TYPE
-  wait_for_fulfillment  = true
+  count                     = var.SPOT_INSTANCE_COUNT  
+  ami                       = data.aws_ami.ami.id
+  instance_type             = var.INSTANCE_TYPE
+  wait_for_fulfillment      = true
+  vpc_security_group_ids    = [aws_security_group.allow_app.id]
+  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}-spot"
+        Name = "${var.COMPONENT}-${var.ENV}-spot"
   }
 }
 
 # Creates On-Demand EC2 
 resource "aws_instance" "od" {
-  count         = var.OD_INSTANCE_COUNT 
-  ami           = data.aws_ami.ami.id
-  instance_type = var.INSTANCE_TYPE
+  count                      = var.OD_INSTANCE_COUNT 
+  ami                        = data.aws_ami.ami.id
+  instance_type              = var.INSTANCE_TYPE
+  vpc_security_group_ids     = [aws_security_group.allow_app.id]
+  subnet_id                  = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}-od"
+        Name = "${var.COMPONENT}-${var.ENV}-od"
   }
 }
