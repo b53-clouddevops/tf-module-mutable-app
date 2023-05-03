a4e3bc7cbd3a31c8de7414a27e0db7f6fe0bd096
CodingManoj <mvp2612@hotmail.com>
2023-05-02 22:17:17 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -6,10 +6,6 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment      = true
   vpc_security_group_ids    = [aws_security_group.allow_app.id]
   subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
-
-  tags = {
-        Name = "${var.COMPONENT}-${var.ENV}-spot"
-  }
 }
 
 # Creates On-Demand EC2 
@@ -21,6 +17,15 @@ resource "aws_instance" "od" {
   subnet_id                  = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
-        Name = "${var.COMPONENT}-${var.ENV}-od"
+        Name = "${var.COMPONENT}-${var.ENV}"   # This tag is for the spot request and not for the spot server
   }
+}
+
+
+# Creates Tags 
+resource "aws_ec2_tag" "ec2_tags" {
+  count       = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
+  resource_id = local.INSTANCE_IDS
+  key         = "Name"
+  value       = "${var.COMPONENT}-${var.ENV}"
 }
