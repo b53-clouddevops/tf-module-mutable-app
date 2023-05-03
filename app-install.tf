c1d410fcc302fc7cbcee642929c739de95c93912
CodingManoj <mvp2612@hotmail.com>
2023-05-02 22:37:19 -0400

    Git Auto Push

--- app-install.tf
+++ app-install.tf
@@ -1,17 +1,17 @@
 resource "null_resource" {
-    count        = var.SPOT_INSTANCE_COUNT  + var.OD_INSTANCE_COUNT  
+   count        = var.SPOT_INSTANCE_COUNT  + var.OD_INSTANCE_COUNT  
 
 # Declaring the remote provisioner inside the resource
-  provisioner "remote-exec" {
-      connection {                   
-        type     = "ssh"
-        user     = local.SSH_USERNAME
-        password = local.SSH_PASSWORD
-        host     = element(local.INSTANCE_IPS, count.index)   
-      }
+    provisioner "remote-exec" {
+        connection {                   
+            type     = "ssh"
+            user     = local.SSH_USERNAME
+            password = local.SSH_PASSWORD
+            host     = element(local.INSTANCE_IPS, count.index)   
+        }
 
-    inline = [
-        "ansible-pull -U https://github.com/b53-clouddevops/ansible.git robot-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT} -e VERSION=${var.APP_VERSION}"
-    ]
-  }
+        inline = [
+            "ansible-pull -U https://github.com/b53-clouddevops/ansible.git robot-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT} -e VERSION=${var.APP_VERSION}"
+        ]
+    }
 } 
