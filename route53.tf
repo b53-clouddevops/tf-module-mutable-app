b898bd58112fc83a2aee0b57662f38f5b5450687
CodingManoj <mvp2612@hotmail.com>
2023-05-03 23:14:09 -0400

    Git Auto Push

---
+++ route53.tf
@@ -0,0 +1,11 @@
+# This created the CNAME Record for Components
+resource "aws_route53_record" "record" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [Private Load Balancer Address]
+}
+
+# A Record     : Creating Name to an IP Address
+# CNAME Record : Creating Name to a Name
