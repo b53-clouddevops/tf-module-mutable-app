b60542d615875aede4584ccc266bccc3bd22c8cd
CodingManoj <mvp2612@hotmail.com>
2023-05-03 22:34:58 -0400

    Git Auto Push

--- tg.tf
+++ tg.tf
@@ -1,15 +1,15 @@
 # Creates a target group
 resource "aws_lb_target_group" "app" {
-  name     = "${var.COMPONENT}-${var.ENV}-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  name              = "${var.COMPONENT}-${var.ENV}-tg"
+  port              = 8080
+  protocol          = "HTTP"
+  vpc_id            = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
 
 # Attaches the component instances to the component target group
 resource "aws_lb_target_group_attachment" "attach_instances" {
-  count            = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT  
-  target_group_arn = aws_lb_target_group.app.arn
-  target_id        = element(local.INSTANCE_IDS, count.index)
-  port             = 8080
+  count             = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT  
+  target_group_arn  = aws_lb_target_group.app.arn
+  target_id         = element(local.INSTANCE_IDS, count.index)
+  port              = 8080
 }
