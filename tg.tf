cc1d24da04485fd9465ef88527f28c060f67f92d
CodingManoj <mvp2612@hotmail.com>
2023-05-04 22:31:08 -0400

    Git Auto Push

--- tg.tf
+++ tg.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 # Adds a rule inside the listener 
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
@@ -30,4 +30,10 @@ resource "aws_lb_listener_rule" "app_rule" {
       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
     }
   }
+}
+
+# Generates a random integar
+resource "random_integer" "priority" {
+  min = 101
+  max = 500
 }
